dad7e74449d52cc5c44a9ad6e1038f778be56830
towhid556 <towhid556@outlook.com>
2023-12-26 23:20:54 +0600

    products  commit

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,13 +4,14 @@ from .serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from .permissions import IsStaffEditorPermission
+from api.authentication import TokenAuthentication
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+    authentication_classes = [authentication.SessionAuthentication, TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         title = serializer.validated_data.get('title')
@@ -113,3 +114,8 @@ product_destroy_view = ProductDeleteView.as_view()
 #
 #
 # product_mixin_view = ProductMixinView.as_view()
+
+
+
+# opu 12jj34jj56jj78jj
+# staff 12uu34uu56uu78uu
--- my-client/list.py
+++ my-client/list.py
@@ -1,9 +1,25 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://localhost:8000/api/products/"
+endpoint = "http://localhost:8000/api/auth/"
+username = input("Give your User Name?")
+password = getpass("Give your password?")
 
-r = requests.get(endpoint)
+
+auth_response = requests.post(endpoint, json={'username': username, 'password': password })
 
 # print(r.text)
-print(r.json())
-print(r.status_code)
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f"Bearer {token}"
+    }
+    endpoint = "http://localhost:8000/api/products/"
+
+    r = requests.get(endpoint, headers=headers)
+
+    # print(r.text)
+    print(r.json())
+    print(r.status_code)
